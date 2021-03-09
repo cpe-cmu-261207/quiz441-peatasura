c31fee3ac4ce34a9538ddece758eac6cb38f70fd
peatasura <keenphantom.2013@gmail.com>
2021-03-09 15:45:38 +0700

    TT

--- index.ts
+++ index.ts
@@ -12,6 +12,11 @@ app.use(cors())
 const PORT = process.env.PORT || 3000
 const SECRET = "SIMPLE_SECRET"
 
+interface DbSchema {
+  users: []
+  
+}
+
 interface JWTPayload {
   username: string;
   password: string;
@@ -22,6 +27,7 @@ app.post('/login',
 
     const { username, password } = req.body
     // Use username and password to create token.
+    
 
     return res.status(200).json({
       message: 'Login succesfully',
@@ -32,6 +38,25 @@ app.post('/register',
   (req, res) => {
 
     const { username, password, firstname, lastname, balance } = req.body
+    const hashPassword = bcrypt.hashSync(password,10)
+    try{
+      const user = await User.create({
+        username,
+      password:hashPassword,
+      firstname,
+      lastname,
+      balance
+      })
+      res.status(200).json(
+      {
+        message:"Register successfully"
+      }
+      )
+    }catch(e){
+      if(e.username)
+    }
+    
+    
   })
 
 app.get('/balance',
@@ -39,10 +64,17 @@ app.get('/balance',
     const token = req.query.token as string
     try {
       const { username } = jwt.verify(token, SECRET) as JWTPayload
+      res.json(token)
   
     }
     catch (e) {
       //response in case of invalid token
+      res.status(401)
+      res.json(
+        {
+          message:"Invalid token"
+        }
+      )
     }
   })
 
@@ -62,6 +94,32 @@ app.post('/withdraw',
 app.delete('/reset', (req, res) => {
 
   //code your database reset here
+  const id = Number(req.params.id)
+  const token = req.query.token as string
+
+  console.log(id)
+
+  try {
+    const data = jwt.verify(token, SECRET) as JWTPayload
+
+    const todo =  data.destroy({ where: { id, userId: data.id } })
+
+    if (todo === 0) {
+      res.status(404)
+      res.json({
+        message: 'This todo not found'
+      })
+      return
+    }
+
+    res.json({
+      message: 'Deleted todo'
+    })
+
+  } catch(e) {
+    res.status(401)
+    res.json({ message: e.message })
+  }
   
   return res.status(200).json({
     message: 'Reset database successfully'
@@ -69,7 +127,11 @@ app.delete('/reset', (req, res) => {
 })
 
 app.get('/me', (req, res) => {
-  
+  return res.status(200).json({
+    "firstname": "Pryat",
+    "lastname" : "Kaewthep",
+    "code" : 620610796,
+    "gpa" : 4.00}) 
 })
 
 app.get('/demo', (req, res) => {
